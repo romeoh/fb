bff472a9001a4e7f02f4a4bba2f83dcd7d0f396b
romeoh78@gmail.com <romeoh78@baeg-ui-MacBook-Pro.local>
2013-05-12 17:26:09 +0900

    Update

--- js/test.js
+++ js/test.js
@@ -100,7 +100,8 @@ https://www.facebook.com/sharer/sharer.php?s=100&p[url]=http://www.naver.com&p[i
 	url += 'description=' + encodeURIComponent(description) + '&'
 	url += 'redirect_uri=' + redirect
 	
-	window.location.href = url
+	//window.location.href = url
+	window.open(url)
 	console.log(url)
 
 	//window.open(url)
